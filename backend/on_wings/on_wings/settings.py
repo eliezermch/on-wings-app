650134475a8957f655951fc3ecd216da144e8617
Eliezer Chirino <emchch3001@gmail.com>
2025-11-29 13:16:40 -0300

    Update: set database config

--- backend/on_wings/on_wings/settings.py
+++ backend/on_wings/on_wings/settings.py
@@ -80,12 +80,23 @@ WSGI_APPLICATION = 'on_wings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Check for DATABASE_URL or POSTGRES_URL (Vercel default)
+database_url = os.environ.get('DATABASE_URL') or os.environ.get('POSTGRES_URL')
+
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
+else:
+    DATABASES['default'] = dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
 
 
 # Password validation
