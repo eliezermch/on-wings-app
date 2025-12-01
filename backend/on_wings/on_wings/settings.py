e6cbdede467695a932c18d631ff0aca0ad69cc74
Eliezer Chirino <emchch3001@gmail.com>
2025-12-01 11:58:19 -0300

    Update: setup static files

--- backend/on_wings/on_wings/settings.py
+++ backend/on_wings/on_wings/settings.py
@@ -135,13 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
